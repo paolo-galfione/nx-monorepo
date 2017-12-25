0e6588774b493848bad920545e5f6352a51d55a2
Paolo Galfione <paolo.galfione@softwareuno.it>
2017-12-25 17:34:56 +0100

    create nx-erp

---
+++ apps/nx-erp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NxModule } from '@nrwl/nx';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent}
+    ], { initialNavigation: 'enabled' })
+  ],
+  declarations: [AppComponent, HomeComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
--- libs/toolbox/src/components/item-detail/item-detail.component.ts
+++ libs/toolbox/src/components/item-detail/item-detail.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnChanges, SimpleChanges, Input } from "@angular/core";
-import { Item } from "@nx-monorepo/toolbox";
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { Item } from '@nx-monorepo/toolbox';
 
 @Component({
-  selector: "app-item-detail",
-  templateUrl: "./item-detail.component.html"
+  selector: 'app-item-detail',
+  templateUrl: './item-detail.component.html'
 })
 export class ItemDetailComponent implements OnChanges {
   @Input() item: Item;
